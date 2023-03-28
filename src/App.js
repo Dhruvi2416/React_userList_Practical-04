f89a0ab744a7c6c3b8ae1b6a35bed787aa847238
Dhruvi2416 <dhruvi.s@simformsolutions.com>
2023-03-28 12:31:36 +0530

    Dynamically user fetch info

--- src/App.js
+++ src/App.js
@@ -1,89 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 export default function App() {
-  const obj0 = {
-    name: "Jana Strassmann",
-    email: "batz.mortimer@yahoo.com",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWBggaQPUlVZTafvGODsbxEsjt_3cKLFyjwA&usqp=CAU",
-    status: "Active",
-    access: "Owner",
-  };
-  const obj1 = {
-    name: "Jube Bowman",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?auto=compress&cs=tinysrgb&w=800",
-    status: "Inactive",
-    access: "Manager",
-  };
-  const obj2 = {
-    name: "Kwak Seong-Min",
-    email: "kale_lehner@yahoo.com",
-    img: "https://i.pinimg.com/736x/60/7a/c7/607ac7108708bddbc549c37fdef374b0.jpg",
-    status: "Inactive",
-    access: "Read",
-  };
-
-  const obj3 = {
-    name: "Lean Hunt",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://qph.cf2.quoracdn.net/main-qimg-2e154de9928d39231ab4526d12d1c1b5-lq",
-    status: "Inactive",
-    access: "Manager",
-  };
-
-  const obj4 = {
-    name: "Nitithorn Prinya",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnpxBJojY1to2PR13mo43pxbnct4a3QVP0aw&usqp=CAU",
-    status: "Inactive",
-    access: "Manager",
-  };
-
-  const obj5 = {
-    name: "Nombeko Mabandia",
-    email: "kale_lehner@yahoo.com",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbGE7lf5hYNb4EppbrOCvrln7Bxufm-AaDGA&usqp=CAU",
-    status: "Inactive",
-    access: "Read",
-  };
-
-  const obj6 = {
-    name: "Nwoye Akachi",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-Fs7gKDmwToX__Y7vHf_nbJmLK3BA6rdBIg&usqp=CAU",
-    status: "Inactive",
-    access: "Manager",
-  };
-
-  const obj7 = {
-    name: "Paulina Gayoso",
-    email: "kale_lehner@yahoo.com",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkohfRidZKKq9kBPDAksr6gK6Ym-AbI3QHkw&usqp=CAU",
-    status: "Inactive",
-    access: "Read",
-  };
-
-  const obj8 = {
-    name: "Phawta Tuntayakul",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRryGMrAhw81ROGThYiDtE-bxTbY6VZKubPcg&usqp=CAU",
-    status: "Inactive",
-    access: "Manager",
-  };
-
-  const obj9 = {
-    name: "Siri Jakobsson",
-    email: "ullrich.owen@kristin.biz",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJrgbjkevk_l2OzmJ1uDbqe9Uj7ZuTbmhIhcv1hiQvXnmN4D1jRsoeCW5JveExgiXjWkM&usqp=CAU",
-    status: "Inactive",
-    access: "Manager",
-  };
-  const arr = [obj0, obj1, obj2, obj3, obj4, obj5, obj6, obj7, obj8, obj9];
-
+  // first empty array and then will set with the data given in api
+  const [arr, setArr] = useState([]);
+  // card display first -1 that is nothingand then will set according to it's index to display particular user
   const [display, setDisplay] = useState(-1);
 
+  const [currentpage, setCurrentPage] = useState(1);
+  const [totalpage, setTotalPage] = useState(0);
 
+  function handlePageChange(i) {
+    console.log(i);
+    setCurrentPage(i);
+    fetch(`https://reqres.in/api/users?page=${i}`)
+      .then((response) => response.json())
+      .then((json) => {
+        setTotalPage(json.total_pages);
+
+        if (i == 1) {
+          json.data[0].access = "Owner";
+          json.data[0].status = "Active";
+          setArr(json.data);
+        } else {
+          setArr(json.data);
+        }
+      });
+  }
+
+  useEffect(() => handlePageChange(1), []);
 
   return (
     <>
@@ -104,7 +49,7 @@ export default function App() {
               </tr>
             </thead>
             <tbody>
-              {arr.map((list, i) => {
+              {arr.map((user_info, i) => {
                 return (
                   <tr className="item_loop " key={i}>
                     <td>
@@ -114,16 +59,18 @@ export default function App() {
                         onMouseLeave={() => setDisplay(-1)}
                       >
                         <div className="profile_pic">
-                          <img src={list.img}></img>
+                          <img src={user_info.avatar}></img>
                         </div>
                         <div className="info">
-                          <h6>{list.name}</h6>
-                          <p>{list.email}</p>
+                          <h6>
+                            {user_info.first_name + " " + user_info.last_name}
+                          </h6>
+                          <p>{user_info.email}</p>
                         </div>
                       </div>
                     </td>
-                   
-                    {list.access == "Owner" ? (
+
+                    {user_info.access == "Owner" ? (
                       <>
                         <td>
                           <div className="owner_status">
@@ -137,7 +84,7 @@ export default function App() {
                         </td>
                         <td>
                           <div className="owner_lock">
-                            <i class="fas fa-lock fa-xl"></i>
+                            <i className="fas fa-lock fa-xl"></i>
                           </div>
                         </td>
                       </>
@@ -154,7 +101,7 @@ export default function App() {
                               data-bs-toggle="dropdown"
                               aria-expanded="false"
                             >
-                              {list.status}
+                              Inactive
                             </button>
                             <ul
                               className="dropdown-menu"
@@ -178,7 +125,7 @@ export default function App() {
                                 data-bs-toggle="dropdown"
                                 aria-expanded="false"
                               >
-                                {list.access}
+                                Manager
                               </button>
                               <ul
                                 className="dropdown-menu"
@@ -203,20 +150,39 @@ export default function App() {
               })}
             </tbody>
           </table>
+
+          <div className="page_navigation">
+            {[...Array(totalpage)].map((i, index) => (
+              <button
+                key={index}
+                disabled={currentpage == index + 1 ? true : false}
+                onClick={() => {
+                  handlePageChange(index + 1);
+                }}
+              >
+                {index + 1}
+              </button>
+            ))}
+          </div>
         </div>
 
         <div className="right col-10 col-sm-10 col-md-10 col-lg-4 col-xl-4 col-xxl-4">
-
-
           {display == -1 ? null : (
             <div className="box">
               <div className="center_content d-flex">
                 <div className="image_right">
-                  <img src={arr[display].img}></img>
+                  <img src={arr[display].avatar}></img>
                 </div>
-                <p>{arr[display].name}</p>
+
+                <div className="name d-flex">
+                  <h4>
+                    {arr[display].first_name + " " + arr[display].last_name}
+                  </h4>
+                  <div className="circle"></div>
+                </div>
+
                 <p>{arr[display].email}</p>
-                <h4>Your Plan: Standard </h4>
+                <h5>Your Plan: Standard </h5>
                 <button>Active User</button>
               </div>
 
